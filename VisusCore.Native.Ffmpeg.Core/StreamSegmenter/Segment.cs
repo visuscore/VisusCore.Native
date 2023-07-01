70943d2865c028400d1e01111307329e3a71c1f9
József Horváth <jozsef.horvath@visuscore.net>
2023-07-01 14:04:06 +0200

    Using ReadOnlySpan instead of IEnumerable

--- VisusCore.Native.Ffmpeg.Core/StreamSegmenter/Segment.cs
+++ VisusCore.Native.Ffmpeg.Core/StreamSegmenter/Segment.cs
@@ -1,11 +1,14 @@
-using System.Collections.Generic;
+using System;
 
 namespace VisusCore.Native.Ffmpeg.Core.StreamSegmenter;
 
 public class Segment
 {
-    public IEnumerable<byte> Init { get; init; }
-    public IEnumerable<byte> Data { get; init; }
+    private readonly byte[] _init;
+    private readonly byte[] _data;
+
+    public ReadOnlySpan<byte> Init { get => _init; init => _init = value.ToArray(); }
+    public ReadOnlySpan<byte> Data { get => _data; init => _data = value.ToArray(); }
     public long? Duration { get; init; }
     public long? TimestampUtc { get; init; }
     public long? TimestampWallclock { get; init; }
